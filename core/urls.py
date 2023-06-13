59da3c8953c74b3c107e5991fdf73d3758fcae7d
Franco Loto <franco.loto1995@gmail.com>
2023-06-13 20:31:23 -0300

    vuelta atras urls core

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
     
     path('admin/', admin.site.urls),
-    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
