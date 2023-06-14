64e0a30365d394eb3656e4df453bd2914ac906ba
Franco Loto <franco.loto1995@gmail.com>
2023-06-13 22:18:10 -0300

    cambios home

--- src/containers/pages/Home.jsx
+++ src/containers/pages/Home.jsx
@@ -7,26 +7,20 @@ import Footer from "components/navigation/Footer"
 import Navbar from "components/navigation/Navbar"
 import Layout from "hocs/layouts/Layout"
 import { useEffect } from "react"
-import { get_categories } from "redux/actions/categories/categories";
 import { connect } from "react-redux";
 import { get_blog_list } from "redux/actions/blog/blog";
 
 
 function Home({
-    get_categories,
-    categories,
     get_blog_list,
     posts
 }){
     useEffect(()=>{
         window.scrollTo(0,0);
-        get_categories();
         get_blog_list();
     },[]);
 
-    if (!categories || !posts) {
-        return null; // O puedes mostrar un spinner de carga en su lugar
-    }
+    
 
     return(
         <Layout>
@@ -44,13 +38,11 @@ function Home({
 }
 
 const mapStateToProps = state => ({
-    categories: state.categories.categories,
     posts: state.blog.blog_list,
 });
 
 
 
 export default connect(mapStateToProps, {
-    get_categories,
     get_blog_list
 })(Home)
