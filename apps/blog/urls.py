ca4dde0167fea5e76c5a372e5efc1ab8cee40910
Franco Loto <franco.loto1995@gmail.com>
2023-10-31 15:30:37 -0300

    cambios urls blog

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,25 +1,15 @@
 from django.urls import path
 
-from .views import (
-    AuthorBlogListView,
-    AuthorPostDetailView,
-    BlogListView,
-    CreateBlogPostView,
-    DeleteBlogPostView,
-    DraftBlogPostView,
-    EditBlogPostView,
-    ListPostByCategoryView,
-    PostDetailView,
-    PublishBlogPostView,
-)
+from .views import (AuthorBlogListView, BlogListView, CreateBlogPostView,
+                    DeleteBlogPostView, DraftBlogPostView, EditBlogPostView,
+                    ListPostByCategoryView, PostDetailView,
+                    PublishBlogPostView)
 
 urlpatterns = [
     path("list", BlogListView.as_view()),
     path("by_category", ListPostByCategoryView.as_view()),
     path("detail/<slug>", PostDetailView.as_view()),
-
     path("author_list", AuthorBlogListView.as_view()),
-    path("author_detail/<post_id>", AuthorPostDetailView.as_view()),
     path("edit", EditBlogPostView.as_view()),
     path("draft", DraftBlogPostView.as_view()),
     path("publish", PublishBlogPostView.as_view()),
